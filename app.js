1155dbfaffeb01d669e984c0650c4fb32db507ed
Yago-melo <106097295+Yago-melo@users.noreply.github.com>
2026-08-04 21:42:33 -0300

    Update API URL to use HTTPS

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://www.Meu-currículo.somee.com/";
+const API_URL = "https://www.Meu-currículo.somee.com/";
 
 async function carregarExperiencias() {
     const container = document.getElementById("lista-experiencias");
